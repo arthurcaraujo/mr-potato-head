bc8c22de0d6693b35dea924d000b26b7239afe3a
arthurcaraujo <arthur-lagamar@hotmail.com>
2023-11-06 19:58:08 -0300

    Add a scroll-to-bottom/top functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,48 @@
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faCircleHalfStroke, faDownLong } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 import { useTheme } from "./ThemeContext";
 import websiteLogo from "../assets/website-logo.png";
 
 export default function Header() {
+    const [scrollDown, setScrollDown] = useState(true);
     const {theme, toggleTheme} = useTheme();
 
+    useEffect(() => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+
+        const handleScrollButton = () => {
+            (window.scrollY > 1000) ? setScrollDown(false) : setScrollDown(true);
+        }
+
+        window.addEventListener("scroll", handleScrollButton);
+
+        return () => {
+            window.removeEventListener("scroll", handleScrollButton);
+        }
+    }, []);
+
+
+    function handleScrollToBottom() {
+        window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: "smooth"
+        });
+    }
+
+    function handleScrollToTop() {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+
     return (
-        <header style={{
-                    backgroundColor: (theme === "dark")
-                        ? "rgb(0, 50, 150)"
-                        : "rgb(0, 128, 255)"
-        }}>
+        <header
+            style={{
+                backgroundColor: (theme === "dark")
+                    ? "rgb(0, 50, 150)"
+                    : "rgb(0, 128, 255)"
+            }}
+        >
             <nav>
                 <a
                     href="https://arthurcaraujo.github.io/"
@@ -23,11 +53,21 @@ export default function Header() {
                     <span id="logo-last-name">C. ARAÚJO</span>
                 </a>
                 <FontAwesomeIcon
+                    icon={faDownLong}
+                    id="arrow-icon"
+                    onClick={scrollDown ? handleScrollToBottom : handleScrollToTop}
+                    style={{
+                        color: (theme === "dark") ? "#fff" : "#000",
+                        transform: scrollDown ? "rotate(0deg)" : "rotate(180deg)",
+                    }}
+                    title={scrollDown ? "Scroll to bottom" : "Scroll to top"}
+                />
+                <FontAwesomeIcon
                     icon={faCircleHalfStroke}
                     id="dark-light-icon"
                     onClick={toggleTheme}
                     style={{
-                        color: (theme === "dark") ? "#000" : "#fff",
+                        color: (theme === "dark") ? "#fff" : "#000",
                         transform: (theme === "dark")
                                         ? "rotate(180deg)"
                                         : "rotate(0deg)",
